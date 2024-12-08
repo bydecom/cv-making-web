dd138badd01bf1564cdf2851faf0ddcedbf4d0ae
Thaibang4903 <152392996+bydecom@users.noreply.github.com>
2024-12-08 21:33:02 +0700

    Fix bug

--- testbackend/callapi.js
+++ testbackend/callapi.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const { GoogleGenerativeAI } = require('@google/generative-ai')
 
-const genAI = new GoogleGenerativeAI(process.env.VITE_GEMINI_API_KEY)
-const model = genAI.getGenerativeModel({ model: process.env.VITE_APP_AI_MODEL })
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY)
+const model = genAI.getGenerativeModel({ model: import.meta.env.VITE_GEMINI_API_KEY })
 
 const enhanceEmail = async (emailContent) => {
   if (!emailContent) {
